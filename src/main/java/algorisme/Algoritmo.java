70fae1bfef3c5e3c32bab4768c37af4799c3ea04
arnau.miret <arnau.miret@estudiantat.upc.edu>
2025-04-10 17:08:45 +0200

    estructura algoritmo 2, casi al completo

--- src/main/java/algorisme/Algoritmo.java
+++ src/main/java/algorisme/Algoritmo.java
@@ -36,7 +36,7 @@ public class Algoritmo {
         // HORIZONTAL Y VERTICALMENTE (TRANSPUESTO)
 
         for(int i = 0; i < 2; i++) {
-            // Modificamos los cross-check sets para no computar con letras qhe no generan palabras válidas
+            // Modificamos los cross-check sets para no computar con letras que no generan palabras válidas
             computarCrossChecks(dawg,tablero,atril);
 
             // Obtenemos las posiciones de las anclas del tablero
@@ -60,43 +60,69 @@ public class Algoritmo {
     }
 
     /**
-     *
+     *  Función que devuelve la mejor palabra que se puede computar en un ancla
      * @param dawg
      * @param tablero
      * @param pos
      * @param atril
      * @return
      */
-    private List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>> computarPalabraAncla(Dawg dawg, Tablero tablero, SimpleEntry<Integer, Integer> pos, String[] atril) {
+    private List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>> computarPalabraAncla(Dawg dawg, Tablero tablero, SimpleEntry<Integer, Integer> ancla, String[] atril) {
         List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>> mejorPalabraAncla = new ArrayList<>();
+
+
 
         //backtracking
 
         // Si hay casilla a la izquierda del ancla, se computa la parte izquierda y luego la derecha
-        int x = pos.getKey();
-        int y = pos.getValue();
+        int x = ancla.getKey();
+        int y = ancla.getValue();
 
-        if(casillaCorrecta(x, y)) {
+        // Si la casilla a la izquierda del ancla queda fuera de los límites, la parte izquierda no se computa
 
+        // en este caso la parte izquierda se computa
+        if(casillaCorrecta(x, y - 1)) {
             // Mirar si la casilla a la izquierda del ancla es vacía o no
-            // Si la casilla esta ocupada
-            if(tablero[x][y].getKey().getKey() != null) {
+            // Si la casilla esta ocupada (parte izq. compuesta de letras ya en el tablero)
+            if(tablero[x][y - 1].getKey().getKey() != null) {
+
+                List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>> parteIzquierda = new ArrayList<>();
+                //List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>> parteDerecha = new ArrayList<>();
 
                 // Computar la parte izquierda con las casillas del tablero
+                int max_long = tamañoParteIzquierdaTablero(tablero,x,y);
+
+                // Funcion que devuelve la parte izquierda ya en el tablero
+                parteIzquierda = computarParteIzquierdaDelTablero(tablero,max_long,x,y);
+
+                // Funcion que devuelve la mejor palabra
+                mejorPalabraAncla = computarParteDerechaTablero(tablero,dawg,parteIzquierda,atril,x,y);
 
             }
 
-            // Si la casilla no esta ocupada
+            // Si la casilla no esta ocupada (parte izq. compuesta de letras del atril)
             else {
 
-                // Computar la parte izquierda con las casillas del atril
+                List<List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>>> partesIzquierdas = new ArrayList<>();
+                //List<List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>>> parteDerecha = new ArrayList<>();
+
                 // Funcion para saber la logitud maxima de la parte izquierda
-                int maxParteIzquierda = tamañoParteIzquierda(tablero, pos);
+                int max_long = tamañoParteIzquierdaAtril(tablero,x,y);
 
                 // Backtracking de las partes izquierdas posibles con las fichas del atril y tamaño indicado
+                partesIzquierdas = computarParteIzquierdaDelAtril(dawg,atril,max_long,x,y);
 
+                // Funcion que devuelve la mejor palabra a partir de las partes izquierdas obtenidas anteriormente
+                mejorPalabraAncla = computarParteDerechaAtril(tablero,dawg,partesIzquierdas,atril,x,y);
 
             }
+
+        }
+
+        // en este caso la parte izquierda NO se computa
+        else{
+            // Computación únicamente de la parte derecha
+            mejorPalabraAncla = computarParteDerechaUnicamente(tablero,dawg,atril,x,y);
 
         }
 
@@ -221,6 +247,83 @@ public class Algoritmo {
     }
 
     /**
+     *  Función que devuelve la parte izquierda de un ancla que ya sabemos que a su izquierda tiene una casilla vacía
+     * @param tablero
+     * @param longitud
+     * @param atril
+     * @param x (fila ancla)
+     * @param y (columna ancla)
+     * @return
+     */
+    private List<List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>>> computarParteIzquierdaDelAtril(Dawg dawg, String[] atril, int longitud, int x, int y) {
+        List<List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>>> partesIzquierdas = new ArrayList<>();
+
+        int size = atril.length;
+
+        // Backtracking para encontrar, dada la maxima longitud, todas las posibles combinaciones correctas
+
+        return partesIzquierdas;
+
+    }
+
+    /**
+     * Función que devuelve la parte izquierda de un ancla cuando ya sabemos que esta está compuesta únicamente por letras ya en el tablero
+     * @param tablero
+     * @param x (fila del ancla)
+     * @param y (columna del ancla)
+     * @return
+     */
+    private List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>> computarParteIzquierdaDelTablero(Tablero tablero, int longitud, int x, int y) {
+        List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>> parteIzquierda = new ArrayList<>();
+
+        // col se coloca en el principio de la parte izquierda y va retrocediendo hasta su última casilla, justo antes del ancla
+        for(int col = y - longitud; col < y; col++) {
+            parteIzquierda.add( new SimpleEntry<>(new SimpleEntry<>(tablero[x][col].getKey().getKey(), false), new SimpleEntry<>(x, col)));
+        }
+
+        return parteIzquierda;
+    }
+
+    /**
+     * Función que devuelve la mejor palabra del ancla a partir de las diferentes partes izquierdas posibles
+     * @param tablero
+     * @param partesIzquierdas
+     * @param atril
+     * @param x
+     * @param y
+     * @return
+     */
+    private List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>> computarParteDerechaAtril(Tablero tablero, Dawg dawg, List<List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>>> partesIzquierdas, String[] atril, int x, int y) {
+
+    }
+
+    /**
+     * Función que devuelve la mejor palabra del ancla a partir de la parte izquierda obtenida del tablero
+     * @param tablero
+     * @param parteIzquierda
+     * @param atril
+     * @param x
+     * @param y
+     * @return
+     */
+    private List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>> computarParteDerechaTablero(Tablero tablero, Dawg dawg,  List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>> parteIzquierda, String[] atril, int x, int y) {
+
+    }
+
+    /**
+     * Función que devuelve únicamente la mejor parte derecha, ya que no es posible tener parte izquierda
+     * @param tablero
+     * @param dawg
+     * @param atril
+     * @param x
+     * @param y
+     * @return
+     */
+    private List<SimpleEntry<SimpleEntry<String, Boolean>, SimpleEntry<Integer, Integer>>> computarParteDerechaUnicamente(Tablero tablero, Dawg dawg, String[] atril, int x, int y) {
+
+    }
+
+    /**
      *
      * @param x
      * @param y
@@ -230,12 +333,22 @@ public class Algoritmo {
         return x >= 0 && x < 15 && y >= 0 && y < 15;
     }
 
-    private int tamañoParteIzquierda(Tablero tablero, SimpleEntry<Integer, Integer> ancla) {
+    private int tamañoParteIzquierdaAtril(Tablero tablero, int x, int y) {
         int size = 0;
-        int y = ancla.getValue();
 
         // Mientras las casillas a la izquierda sean correctas y no esten ocupadas, sumar uno a size
-        for(int x = ancla.getKey() - 1; tablero[x][y].getKey().getKey() != null && casillaCorrecta(x, y); x--) {
+        for(int col = y - 1; tablero[x][col].getKey().getKey() == null && casillaCorrecta(x, col); col--) {
+            ++size;
+        }
+
+        return size;
+    }
+
+    private int tamañoParteIzquierdaTablero(Tablero tablero, int x, int y) {
+        int size = 0;
+
+        // Mientras las casillas a la izquierda sean correctas y esten ocupadas, sumar uno a size
+        for(int col = y - 1; tablero[x][col].getKey().getKey() != null && casillaCorrecta(x, col); col--) {
             ++size;
         }
 
